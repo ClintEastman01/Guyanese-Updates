b14c2f2d7180cd06c361d3133b38e23e23155c0d
KTAppDev <kentaylorappdev@gmail.com>
2020-12-20 03:09:46 -0400

    Ver 1.7 this is the one. all but the last agency is on database

--- check_percentage.py
+++ check_percentage.py
@@ -8,7 +8,6 @@ percent_of_words_matched = 0
 
 
 def remove_simple_words(words):
-    list_of_simple_words = get_words()
     for simple_word in str(database_read_simplewords()).split():
         for word in words:
             if word.lower() == simple_word.lower():
